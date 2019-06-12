5fd3f5444e5f247392c63a0247f6166c20fd92fe
dearblue <dearblue@users.noreply.github.com>
2019-06-12 21:27:53 +0900

    互換関数 `mrb_to_str()` を mruby-aux 送り

--- src/compat.h
+++ src/compat.h
@@ -10,15 +10,4 @@
 # endif
 #endif
 
-#include <mruby.h>
-#include <mruby/string.h>
-
-#if MRUBY_RELEASE_NO < 20000
-static mrb_value
-mrb_to_str(mrb_state *mrb, mrb_value obj)
-{
-    return mrb_str_to_str(mrb, obj);
-}
-#endif
-
 #endif /* MRUBY_REQUIRE_PLUS_COMPAT_H */
