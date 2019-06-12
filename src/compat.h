6b049b693fdc238795d94664a69a936b04053c39
dearblue <dearblue@users.noreply.github.com>
2019-06-12 21:49:07 +0900

    後方互換のための `MRB_PROC_SET_TARGET_CLASS()` マクロを mruby-aux 送り

--- src/compat.h
+++ src/compat.h
@@ -1,13 +1,6 @@
 #ifndef MRUBY_REQUIRE_PLUS_COMPAT_H
 #define MRUBY_REQUIRE_PLUS_COMPAT_H 1
 
-#include <mruby.h>
-#include <mruby/proc.h>
-
-#if MRUBY_RELEASE_NO < 10400
-# ifndef MRB_PROC_SET_TARGET_CLASS
-#  define MRB_PROC_SET_TARGET_CLASS(PROC, CLASS) ((PROC)->target_class = (CLASS))
-# endif
-#endif
+#include "mruby-aux/compat/proc.h"
 
 #endif /* MRUBY_REQUIRE_PLUS_COMPAT_H */
